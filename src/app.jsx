a0e083ab96cb5e2d1ccd78c2380001d43ee36006
Aleksandr Kondratev <sania.kond@mail.ru>
2024-11-19 18:14:49 +0400

    Исправления домашнего задания 3

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,13 @@
-import { RestaurantsPage } from "./components/restaurants-page/restaurants-page";
+import { restaurants } from "../materials/mock.js";
+import { RestaurantsTabs } from "./components/restaurantsTabs/restaurantsTabs.jsx";
+import { Layout } from "./components/layout/layout.jsx";
 
 export const App = () => {
-  return <RestaurantsPage />;
+  return (
+    <Layout title={"Список ресторанов"}>
+      {Array.isArray(restaurants) && restaurants.length > 0 && (
+        <RestaurantsTabs restaurants={restaurants} />
+      )}
+    </Layout>
+  );
 };
