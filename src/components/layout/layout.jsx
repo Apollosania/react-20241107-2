3bcf895b53d997241118b89390f689727910744b
Aleksandr Kondratev <sania.kond@mail.ru>
2024-12-09 17:51:45 +0400

    Домашнее задание 6: правки, перенос компонетов с использованием контекста темы и авторизации из layout

--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -1,38 +1,27 @@
+import style from "./layout.module.css";
 import { ProgressBar } from "../progress-bar/progress-bar.jsx";
 import { Title } from "../typography/title";
 import { Text } from "../typography/text.jsx";
-import style from "./layout.module.css";
 import { ThemeSwitcher } from "../theme-switcher/theme-switcher";
-import { useTheme } from "../theme-context/use-theme.js";
-import classNames from "classnames";
-import { useAuth } from "../auth-context/use-auth.js";
-import { LoginForm } from "../login-form/login-form";
-import { AccountInfo } from "../account-info/account-info";
+import { AuthWidget } from "../auth-widget/auth-widget.jsx";
+import { Header } from "../header/header";
 
 export const Layout = ({ title, children }) => {
-  const { theme } = useTheme();
-  const { auth } = useAuth();
-
   return (
     <>
       <ProgressBar />
       <div className={style.wrapper}>
-        <header
-          className={classNames(style.header, {
-            [style.headerLight]: theme === "light",
-            [style.headerDark]: theme === "dark",
-          })}
-        >
+        <Header>
           <div className={style.container}>
             <div className={style.topPanel}>
               <ThemeSwitcher />
 
-              {auth.isAuthorized ? <AccountInfo /> : <LoginForm />}
+              <AuthWidget />
             </div>
 
             {Boolean(title) && <Title type={"reverse"}>{title}</Title>}
           </div>
-        </header>
+        </Header>
 
         <main className={style.main}>
           <div className={style.container}>{children}</div>
